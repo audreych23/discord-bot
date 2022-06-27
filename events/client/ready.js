dd0de35dafa269259c289e15e68465d361caafb4
audreych <audreychung2352002@gmail.com>
2022-06-28 00:25:10 +0800

    added database in in ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const { Database } = require('../../config.json');
+
 module.exports = {
   name: 'ready',
   once: true,
@@ -7,5 +10,15 @@ module.exports = {
   execute(client) {
     console.log('The client is now ready');
     client.user.setActivity('Coding in the dungeon');
+
+    if (!Database) return;
+    mongoose.connect(Database, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => {
+      console.log('The client is now connected to the database!');
+    }).catch((err) => {
+      console.log(err);
+    });
   },
 };
